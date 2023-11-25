2eba296211fbd939c55c595eb03969b3d27d7a9b
Diogo Pinela <diogoid7400@gmail.com>
2023-11-25 18:52:38 +0000

    Change the popup after Avarices to reflect the given item

--- ItemChanger/Locations/AvariceLocation.cs
+++ ItemChanger/Locations/AvariceLocation.cs
@@ -17,9 +17,47 @@ internal class AvariceLocation : Location
             {
                 return true;
             }
-            CornerPopup.Show(item.DisplayName);
             item.Trigger();
             return false;
+        }
+    }
+
+    [HL.HarmonyPatch(typeof(NPCCharacter), nameof(NPCCharacter.Trigger))]
+    private static class TextboxPatch
+    {
+        private static void Prefix(NPCCharacter __instance)
+        {
+            if (__instance.cutscene == null)
+            {
+                return;
+            }
+            var key = __instance.cutscene.GetComponent<BaseKey>();
+            if (key == null)
+            {
+                return;
+            }
+
+            void ModifyAvaricePopup(string powerId)
+            {
+                if (ItemChangerPlugin.TryGetPlacedItem(typeof(AvariceLocation), powerId, out var item))
+                {
+                    SpeechPopup.Modify(__instance, $"{item.DisplayName.ToUpper()} ACQUIRED");
+                }
+            }
+
+
+            switch (key.uniqueId)
+            {
+                case "gift_hookshot":
+                    ModifyAvaricePopup("hookshot");
+                    break;
+                case "gift_bombs":
+                    ModifyAvaricePopup("bombs");
+                    break;
+                case "gift_fire":
+                    ModifyAvaricePopup("fire");
+                    break;
+            }
         }
     }
 }
