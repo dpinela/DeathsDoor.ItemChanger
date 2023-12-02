6a32974dff99655f3f15edea9eae4aaf629a6503
Diogo Pinela <diogoid7400@gmail.com>
2023-12-02 21:23:23 +0000

    Send player back to Hall of Doors when leaving Avarice without having the door unlocked

--- ItemChanger/Locations/AvariceLocation.cs
+++ ItemChanger/Locations/AvariceLocation.cs
@@ -60,4 +60,38 @@ internal class AvariceLocation : Location
             }
         }
     }
+
+    // If the player enters an Avarice without having the respective big door open,
+    // die and choose to leave, they should return to Hall of Doors rather than the
+    // room where the big door is in.
+    // This is impossible in a vanilla game, but can happen if they obtain enough
+    // items to access that Avarice directly from Hall of Doors instead:
+    // Hookshot for Avarice 3, or Bomb plus the exit lever for Avarice 2.
+    [HL.HarmonyPatch(typeof(RespawnPrompter), nameof(RespawnPrompter.Leave))]
+    private static class LeavePatch
+    {
+        private static void Prefix(RespawnPrompter __instance)
+        {
+            void RedirectIfLocked(string s)
+            {
+                if (!GameSave.GetSaveData().IsKeyUnlocked(s))
+                {
+                    __instance.returnSceneId = "lvl_HallOfDoors";
+                }
+            }
+
+            switch (__instance.returnDoorId)
+            {
+                case "hod_anc_mansion":
+                    RedirectIfLocked("anc_door_mansion_unlocked");
+                    break;
+                case "hod_anc_forest":
+                    RedirectIfLocked("anc_door_forest_unlocked");
+                    break;
+                case "hod_anc_fortress":
+                    RedirectIfLocked("anc_door_fortress_unlocked");
+                    break;
+            }
+        }
+    }
 }
