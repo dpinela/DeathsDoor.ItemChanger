8f5cf06270709f2dde3a8f99203d0b2290e898bc
Diogo Pinela <diogoid7400@gmail.com>
2024-03-29 15:12:16 +0000

    Add method to get area names

--- ItemChanger/Area.cs
+++ ItemChanger/Area.cs
@@ -21,3 +21,34 @@ public enum Area
     Watchtowers,
     Betty
 }
+
+public static class AreaName
+{
+    private static string[] names = new string[]
+    {
+        "Other",
+        "Hall of Doors",
+        "Grove of Spirits",
+        "Lost Cemetery",
+        "Estate of the Urn Witch",
+        "Ceramic Manor",
+        "Furnace Observation Rooms",
+        "Inner Furnace",
+        "The Urn Witch's Laboratory",
+        "Overgrown Ruins",
+        "Mushroom Dungeon",
+        "Flooded Fortress",
+        "Throne of the Frog King",
+        "Stranded Sailor",
+        "Castle Lockstone",
+        "Camp of the Free Crows",
+        "Old Watchtowers",
+        "Betty's Lair"
+    };
+
+    public static string Of(Area a)
+    {
+        var i = (int)a;
+        return i >= 0 && i < names.Length ? names[i] : names[0];
+    }
+}
