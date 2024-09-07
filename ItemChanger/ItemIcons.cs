da972a710cd27b3adb768e6a6d25fac9199e43ea
Diogo Pinela <diogoid7400@gmail.com>
2024-09-07 01:47:14 +0100

    Add ability to add more icon lookup paths

--- ItemChanger/ItemIcons.cs
+++ ItemChanger/ItemIcons.cs
@@ -1,12 +1,22 @@
 using UE = UnityEngine;
 using IO = System.IO;
-using Collections = System.Collections.Generic;
+using CG = System.Collections.Generic;
 
 namespace DDoor.ItemChanger;
 
 public static class ItemIcons
 {
-    private static Collections.Dictionary<string, UE.Sprite> sprites = new();
+    private static CG.Dictionary<string, UE.Sprite> sprites = new();
+
+    private static CG.List<string> iconPaths = new()
+    {
+        IO.Path.GetDirectoryName(typeof(ItemIcons).Assembly.Location)
+    };
+
+    public static void AddPath(string dir)
+    {
+        iconPaths.Add(dir);
+    }
 
     public static UE.Sprite Get(string name)
     {
@@ -22,11 +32,23 @@ public static class ItemIcons
 
     private static UE.Sprite LoadSprite(string name)
     {
-        var loc = IO.Path.Combine(IO.Path.GetDirectoryName(typeof(ItemIcons).Assembly.Location), name);
-        var imageData = IO.File.ReadAllBytes(loc);
-        var tex = new UE.Texture2D(1, 1, UE.TextureFormat.RGBA32, false);
-        UE.ImageConversion.LoadImage(tex, imageData, true);
-        tex.filterMode = UE.FilterMode.Bilinear;
-        return UE.Sprite.Create(tex, new UE.Rect(0, 0, tex.width, tex.height), new UE.Vector2(.5f, .5f));
+        foreach (var p in iconPaths)
+        {
+            var loc = IO.Path.Combine(p, name);
+            byte[] imageData;
+            try
+            {
+                imageData = IO.File.ReadAllBytes(loc);
+            }
+            catch (IO.FileNotFoundException)
+            {
+                continue;
+            }
+            var tex = new UE.Texture2D(1, 1, UE.TextureFormat.RGBA32, false);
+            UE.ImageConversion.LoadImage(tex, imageData, true);
+            tex.filterMode = UE.FilterMode.Bilinear;
+            return UE.Sprite.Create(tex, new UE.Rect(0, 0, tex.width, tex.height), new UE.Vector2(.5f, .5f));
+        }
+        throw new System.InvalidOperationException($"sprite {name} not found");
     }
 }
